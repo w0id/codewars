e71cc39941e6db88c900c700510d0ea8ab49a58d
W0iD <xcrypter@gmail.com>
2022-01-06 13:17:52 +0300

    third commit

--- src/main/java/are_they_the_same/AreSame.java
+++ src/main/java/are_they_the_same/AreSame.java
@@ -1,20 +1,28 @@
 package are_they_the_same;
 
+import java.util.stream.IntStream;
+
 public class AreSame {
     public static boolean comp(int[] a, int[] b) {
-        boolean result = true;
-        int iter = 0;
-        double squire = 0;
-        for(int i = 0; i < b.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (Math.sqrt(b[i]) == a[j]) break;
-                iter++;
-            }
-            if (iter == a.length) {
-                result = false;
+        boolean result;
+        result = !(a == null || b == null);
+        if (result) {
+            result = !(a.length == 0 || b.length == 0);
+        }else{
+            return result;
+        }
+        for (final int current : a) {
+            if (result) {
+                result = IntStream.of(b).anyMatch(x -> x == Math.pow(current, 2));
+            } else {
                 break;
-            }else{
-                iter = 0;
+            }
+        }
+        for (final int current : b) {
+            if (result) {
+                result = IntStream.of(a).anyMatch(x -> x == Math.sqrt(current));
+            } else {
+                break;
             }
         }
         return result;
--- src/main/java/are_they_the_same/AreSameTest.java
+++ src/main/java/are_they_the_same/AreSameTest.java
@@ -4,6 +4,8 @@ public class AreSameTest {
 
     public static void main(String[] args) {
         int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+//        int[] a = null;
+//        int[] b = {};
         int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
         System.out.println(AreSame.comp(a, b));
     }
